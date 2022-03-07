4d7601e34d0d4cd88d014b29b810b53093c8e239
aleksei <alexey3017@mail.ru>
2022-03-07 10:00:41 +0300

    react hook form reset

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./react-router/App";
+import App from "./tutorial-8/App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-
 
 ReactDOM.render(
   <React.StrictMode>
